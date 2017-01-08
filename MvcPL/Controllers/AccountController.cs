9254cb7c141a64483a89fa7723601fad14553384
Shilo Denis <shilo.denis.1997g@gmail.com>
2017-01-08 22:35:19 +0300

    First version of register added

--- MvcPL/Controllers/AccountController.cs
+++ MvcPL/Controllers/AccountController.cs
@@ -1,17 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.Helpers;
 using System.Web.Mvc;
 using System.Web.Security;
+using MvcPL.Infrastructure;
 using MvcPL.Models;
+using MvcPL.Providers;
+using ORM;
 
 namespace MvcPL.Controllers
 {
     public class AccountController : Controller
     {
-        // GET: Account
+        BlogHostModel db = new BlogHostModel();
+
         [HttpGet]
         public ActionResult Login(string returnUrl)
         {
@@ -20,7 +26,8 @@ namespace MvcPL.Controllers
         }
 
         [HttpPost]
-        public ActionResult Login(LogInViewModel viewModel, string returnUrl)
+        [ValidateAntiForgeryToken]
+        public ActionResult LogIn(LogInViewModel viewModel, string returnUrl)
         {
             if (ModelState.IsValid)
             {
@@ -42,6 +49,85 @@ namespace MvcPL.Controllers
                 }
             }
             return View(viewModel);
+        }
+
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("LogIn", "Account");
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterViewModel viewModel)
+        {
+            if (viewModel.Captcha != (string) Session[CaptchaImage.CaptchaValueKey])
+            {
+                ModelState.AddModelError("Captcha", "Incorrect input");
+                return View(viewModel);
+            }
+
+            var anyUser = db.Users.FirstOrDefault(u => u.Login.Equals(viewModel.Login) 
+                || u.Email.Equals(viewModel.Email));
+            if (anyUser != null)
+            {
+                if (anyUser.Email.Equals(viewModel.Email))
+                {
+                    ModelState.AddModelError("Email", "User with this email already exists");
+                }
+                if (anyUser.Login.Equals(viewModel.Login))
+                {
+                    ModelState.AddModelError("Login", "User with this login already exists");
+                }
+                return View(viewModel);
+            }
+            if (ModelState.IsValid)
+            {
+                var membershipUser = ((UserMembershipProvider) Membership.Provider)
+                    .CreateUser(viewModel.Login, viewModel.Firstname, viewModel.Secondname,
+                        viewModel.Thirdname, viewModel.Email, viewModel.Password);
+                if (membershipUser != null)
+                {
+                    FormsAuthentication.SetAuthCookie(viewModel.Email, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Error registration");
+                }
+            }
+            return View(viewModel);
+        }
+
+        //В сессии создаем случайное число от 1111 до 9999.
+        //Создаем в ci объект CatchaImage
+        //Очищаем поток вывода
+        //Задаем header для mime-типа этого http-ответа будет "image/jpeg" т.е. картинка формата jpeg.
+        //Сохраняем bitmap в выходной поток с форматом ImageFormat.Jpeg
+        //Освобождаем ресурсы Bitmap
+        //Возвращаем null, так как основная информация уже передана в поток вывод
+        public ActionResult Captcha()
+        {
+            Session[CaptchaImage.CaptchaValueKey] =
+                new Random(DateTime.Now.Millisecond).Next(1111, 9999).ToString(CultureInfo.InvariantCulture);
+            var ci = new CaptchaImage(Session[CaptchaImage.CaptchaValueKey].ToString(), 211, 50, "Helvetica");
+
+            // Change the response headers to output a JPEG image.
+            this.Response.Clear();
+            this.Response.ContentType = "image/jpeg";
+
+            // Write the image to the response stream in JPEG format.
+            ci.Image.Save(this.Response.OutputStream, ImageFormat.Jpeg);
+
+            // Dispose of the CAPTCHA image object.
+            ci.Dispose();
+            return null;
         }
     }
 }
