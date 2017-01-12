9de1a0ea38b240b543c9193d366ad1fd9c864c36
Shilo Denis <shilo.denis.1997g@gmail.com>
2017-01-12 13:51:02 +0300

    logger interface added

---
+++ LoggerInterfaces/ILogger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerInterfaces
+{
+    /// <summary>
+    /// Provides most popular methods of loggers
+    /// </summary>
+    public interface ILogger
+    {
+        void Debug(string message);
+        void Debug(string message, params object[] args);
+        void Debug(Exception exception, string message, params object[] args);
+        void Error(string message);
+        void Error(string message, params object[] args);
+        void Error(Exception exception, string message, params object[] args);
+        void Fatal(string message);
+        void Fatal(string message, params object[] args);
+        void Fatal(Exception exception, string message, params object[] args);
+        void Info(string message);
+        void Info(string message, params object[] args);
+        void Info(Exception exception, string message, params object[] args);
+        void Warn(string message);
+        void Warn(string message, params object[] args);
+        void Warn(Exception exception, string message, params object[] args);
+        void Trace(string message);
+        void Trace(string message, params object[] args);
+        void Trace(Exception exception, string message, params object[] args);
+    }
+}
