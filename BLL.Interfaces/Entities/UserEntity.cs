5a8c92479a0b47df8c406fed1123bb26098d2987
Shilo Denis <shilo.denis.1997g@gmail.com>
2017-01-23 11:09:36 +0300

    BLL.Interfaces first interfaces created

---
+++ BLL.Interfaces/Entities/UserEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces.Entities
+{
+    public class UserEntity
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Firstname { get; set; }
+        public string Secondname { get; set; }
+        public string Thirdname { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public DateTime DateRegistered { get; set; }
+
+        public ICollection<string> Roles { get; set; }
+    }
+}
---
+++ BLL.Interfaces/Services/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL.Interfaces.Entities;
+
+namespace BLL.Interfaces.Services
+{
+    interface IService
+    {
+        UserEntity GetUserEntity(int id);
+        IEnumerable<UserEntity> GetAllUserEntities();
+        void CreateUser(UserEntity user);
+        void DeleteUser(UserEntity user);
+        void UpdateUser(UserEntity user);
+    }
+}
