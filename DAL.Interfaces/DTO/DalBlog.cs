17b48d030a984d7751b8d8750b04d221e9fec9fd
Shilo Denis <shilo.denis.1997g@gmail.com>
2017-01-24 15:48:26 +0300

    DalBlog created

---
+++ DAL.Interfaces/DTO/DalBlog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces.DTO
+{
+    public class DalBlog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Rating { get; set; }
+        public DateTime DateStarted { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+    }
+}
