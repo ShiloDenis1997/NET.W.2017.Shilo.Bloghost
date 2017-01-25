e022c5fc7793bd8b8bcd6f1c6f7334b2d0c175e1
Shilo Denis <shilo.denis.1997g@gmail.com>
2017-01-25 17:43:52 +0300

    IArticleService created

---
+++ BLL.Interfaces/Services/IArticleService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using BLL.Interfaces.Entities;
+
+namespace BLL.Interfaces.Services
+{
+    public interface IArticleService
+    {
+        ArticleEntity GetArticleEntity(int id);
+
+        IEnumerable<ArticleEntity> GetArticleEntities(int takeCount, int skipCount = 0,
+            Expression<Func<ArticleEntity, int>> orderSelector = null);
+
+        IEnumerable<ArticleEntity> GetArticlesByPredicate
+        (Expression<Func<ArticleEntity, bool>> predicate,
+            int takeCount, int skipCount = 0,
+            Expression<Func<ArticleEntity, int>> orderSelector = null);
+
+        IEnumerable<ArticleEntity> GetArticlesByCreationDate
+            (int takeCount, int skipCount = 0, bool ascending = true);
+
+        BlogEntity GetByPredicate(Expression<Func<ArticleEntity, bool>> predicate);
+
+        void CreateArticle(ArticleEntity article);
+        void DeleteArticle(ArticleEntity article);
+        void UpdateArticle(ArticleEntity article);
+    }
+}
