aa78dfb7572fb1e5e42993f8cb1785b6125458ff
Shilo Denis <shilo.denis.1997g@gmail.com>
2017-01-26 21:33:09 +0300

    DalComments and ICommentsRepository created

---
+++ DAL.Interfaces/Repository/ICommentsRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Interfaces.DTO;
+
+namespace DAL.Interfaces.Repository
+{
+    public interface ICommentsRepository : IRepository<DalComment>
+    {
+        IEnumerable<DalComment> GetCommentsByCreationDate
+            (int takeCount, int skipCount = 0, bool ascending = false);
+    }
+}
