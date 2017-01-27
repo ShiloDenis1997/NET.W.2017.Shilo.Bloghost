b97758fd3abff25c1bcad157ddbe6c468cd83ed5
Shilo Denis <shilo.denis.1997g@gmail.com>
2017-01-27 19:48:13 +0300

    brute tags editing created

--- DAL.Concrete/ArticleRepository.cs
+++ DAL.Concrete/ArticleRepository.cs
@@ -128,6 +128,16 @@ namespace DAL.Concrete
             ormArticle.DateAdded = dalArticle.DateAdded;
             ormArticle.Name = dalArticle.Name;
             ormArticle.Rating = dalArticle.Rating;
+            IEnumerable<Tag> tags =
+                dalArticle.Tags.Where(tag => !string.IsNullOrWhiteSpace(tag))
+                .Select(tag => context.Set<Tag>().FirstOrDefault(t => t.Name.Equals(tag))
+                                ?? context.Set<Tag>().Add(new Tag { Name = tag }));
+            ormArticle.Tags.Clear();
+            foreach (var tag in tags)
+            {
+                if (ormArticle.Tags.All(t => t.Id != tag.Id))
+                    ormArticle.Tags.Add(tag);
+            }
             context.Entry(ormArticle).State = EntityState.Modified;
         }
     }
