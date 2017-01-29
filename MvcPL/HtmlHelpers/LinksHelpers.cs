8cf36b5f1c970aa08a73b6afc9fe61f18dd0a57b
Shilo Denis <shilo.denis.1997g@gmail.com>
2017-01-29 18:05:05 +0300

    packages updated, article ang blog partials now much more beautiful

---
+++ MvcPL/HtmlHelpers/LinksHelpers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcPL.HtmlHelpers
+{
+    public static class LinksHelpers
+    {
+        public static MvcHtmlString GlyphLink
+            (this HtmlHelper html, string glyphClass, string text, Func<string> pageUrl)
+        {
+            TagBuilder span = new TagBuilder("span");
+            span.AddCssClass(glyphClass);
+
+            TagBuilder button = new TagBuilder("button");
+            button.MergeAttribute("type", "button");
+            button.AddCssClass("btn btn-link btn-lg");
+            button.AddCssClass("nodecor");
+            button.InnerHtml = (span + text);
+
+            TagBuilder aTag = new TagBuilder("a");
+            aTag.MergeAttribute("href", pageUrl());
+            aTag.InnerHtml = button.ToString();
+
+            return MvcHtmlString.Create(aTag.ToString());
+        }
+    }
+}
